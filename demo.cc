974ce9141e12ee3eaba5a98232944a3a8a95bc7b
Lux <luh4869@163.com>
2022-10-11 20:58:48 +0800

    adjust some commemts and add an function object demo

--- demo.cc
+++ demo.cc
@@ -17,12 +17,27 @@ void recur_defer() {
   use_defer();
 }
 
+class Obj {
+ private:
+  int count_;
+  double val_;
+
+ public:
+  Obj() : count_(10), val_(1){};
+  void operator()(double val) {
+    val_ = val;
+    cout << "function obj-" << val_ / count_ << endl;
+    ++count_;
+  }
+};
+
 int main() {
   DEFER(test_func, 1, "main");
   use_defer();
   DEFER(test_func, 5, "main");
   recur_defer();
   DEFER([]() { cout << "lambda" << endl; });
+  DEFER(Obj(), 22);
 
   return 0;
 }
